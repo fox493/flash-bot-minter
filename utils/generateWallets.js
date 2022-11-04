1d74c40439880461dbf90fa2c71da5227f1a2c20
TimGrey998 <timgrey998@gmail.com>
2022-11-04 14:37:22 +0800

    feat: change generating path + change distribution amount

--- utils/generateWallets.js
+++ utils/generateWallets.js
@@ -1,20 +1,32 @@
-import { Wallet } from "ethers";
-import fs from 'fs'
-const mnemonic = Wallet.createRandom().mnemonic
-let accounts = {
-    mnemonic: mnemonic.phrase,
-    path: mnemonic.path,
-    addresses: []
+import { ethers } from "ethers"
+import fs from "fs"
+
+const wallet = ethers.Wallet.createRandom()
+
+const data = {
+  mnemonic: wallet.mnemonic.phrase,
+  accounts: [],
 }
-for(let i = 0; i < 50; i++) {
-    let new_wallet = Wallet.fromMnemonic(mnemonic.phrase, mnemonic.path+'/'+i)
-    accounts.addresses.push({
-        public_key: new_wallet.address,
-        private_key: new_wallet.privateKey
-    })
+
+const HDNode = ethers.utils.HDNode.fromMnemonic(wallet.mnemonic.phrase)
+
+for (let i = 0; i < 100; i++) {
+  const node = HDNode.derivePath(`m/44'/60'/0'/0/${i}`)
+  data.accounts.push({
+    pub: node.address,
+    pri: node.privateKey,
+  })
 }
-if(fs.existsSync('./accounts.json')) {
-    console.log('already exist')
-    process.exit(1)
+
+if (!fs.existsSync("accounts.json")) {
+  fs.writeFile("./accounts.json", JSON.stringify(data, 0, 4), (err) => {
+    if (err) console.error(err)
+    else {
+      console.log("🤩 100 accounts generated successfully!")
+    }
+  })
+} else {
+  console.log(
+    "You've already generated 100 accounts, are you sure to generate a new one?\nIf you want please delete accounts.json, and remember to backup the mnemonic first⚠️!!!"
+  )
 }
-fs.writeFileSync('./accounts.json', JSON.stringify(accounts))
--- utils/transferToWallet_v2.js
+++ utils/transferToWallet_v2.js
@@ -1,53 +1,49 @@
 import { FlashbotsBundleProvider } from "@flashbots/ethers-provider-bundle"
-import { ethers, providers, Wallet } from "ethers"
+import { ethers, providers, utils, Wallet } from "ethers"
 import dotenv from "dotenv"
 import fs from "fs"
 dotenv.config(".env")
-const provider = new providers.WebSocketProvider(process.env.TESTNET_RPC_URL)
+const provider = new providers.WebSocketProvider(process.env.MAINNET_RPC_URL)
 const authSigner = Wallet.createRandom()
 // const signer = new Wallet(process.env.PRIVATE_KEY)
 
-const accounts = JSON.parse(fs.readFileSync("./accounts.json")).addresses
-
+const accounts = JSON.parse(fs.readFileSync("./accounts.json")).accounts
 const main = async () => {
   const flashbot_provider = await FlashbotsBundleProvider.create(
     provider,
-    authSigner,
-    "https://relay-goerli.flashbots.net"
+    authSigner
   )
   let tx_bundle = []
   console.log("📦 packing...")
   console.time("packed!")
-  let signer = new Wallet(process.env.PRIVATE_KEY, provider)        
+  let signer = new Wallet(process.env.PRIVATE_KEY, provider)
   let nonce = await signer.getTransactionCount()
-  for (let i = 0; i < 20; i++) {
+  for (let i = 90; i < 100; i++) {
     let account = accounts[i]
     let tx = {
       transaction: {
-        // chainId: 1,
-        chainId: 5,
+        chainId: 1,
         type: 2,
-        value: 0,
+        value: ethers.utils.parseEther("0.003"),
         gasLimit: 21000,
-        maxFeePerGas: ethers.utils.parseUnits("50", "gwei"),
+        maxFeePerGas: ethers.utils.parseUnits("20", "gwei"),
         // maxFeePerGas: ethers.BigNumber.from(10).pow(9).mul(30),
         maxPriorityFeePerGas: ethers.utils.parseUnits("1.5", "gwei"),
-        to: account,
-        nonce: nonce++
+        to: account.pub,
+        nonce: nonce++,
       },
       signer,
     }
     tx_bundle.push(tx)
   }
   console.timeEnd("packed!")
-
   provider.on("block", async (block_number) => {
     console.log(block_number)
 
     const res = await flashbot_provider.sendBundle(tx_bundle, block_number + 1)
 
     if ("error" in res) {
-      console.error(res.error.message)
+      console.error(res.error)
       return
     }
     console.log(await res.simulate())
